da9c4ace2c6065920feeb44b0e9f1a288cf40a1f
johnnysuh <nicolas_c_duarte@estudante.sesisenai.org.br>
2024-09-30 22:19:38 -0300

    first commit

---
+++ app/Tela Inicial IFome/ItemIfome/index.jsx
@@ -0,0 +1,66 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native"
+import { AppContext } from "../../../scripts/AppContext";
+import { useContext } from "react";
+
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+        height: 290,
+        borderRadius: 10,
+        padding: 5,
+        marginBottom: 10,
+        width: "90%",
+        alignSelf: 'center',
+        marginTop: 10,
+    },
+    nome: {
+        alignSelf: "flex-start",
+        fontSize: 15,
+        fontWeight: "bold",
+    },
+    local: {
+        alignSelf: "flex-start",
+        fontSize: 12,
+        color: "#afafaf"
+    }
+
+
+
+})
+
+
+export default ItemIfome = ({ nome, imagem, preco, local }) => {
+    const {carrinho, setCarrinho} = useContext(AppContext)
+
+
+
+    return (
+        <View style={styles.container}>
+            <Image
+                style={{ width: "90%", height: 220, resizeMode: 'contain', borderRadius: 5 }}
+                source={{
+                    uri: imagem,
+                }}
+            />
+            <View style={{ display: 'flex', flexDirection: "row", justifyContent: 'space-between', alignItems: "center", width: '90%' }}>
+                <View>
+                    <Text style={styles.nome}>{nome}</Text>
+                    <Text style={styles.local}>{local}</Text>
+                    <Text style={styles.local}>R${preco.toFixed(2)}</Text>
+                </View>
+                <Pressable onPress={() => {
+                        var carrinhoCopia = [...carrinho]
+                        carrinhoCopia.push({"nome": nome, "local": local, "preco": preco})
+                        setCarrinho(carrinhoCopia)
+}
+                    } style={{backgroundColor: "#ffffff", padding: 5, borderRadius: 5, borderColor: "#ff0000", borderWidth: 1}}><Text style={{color: '#ff0000'}}>COMPRAR</Text></Pressable>
+            </View>
+
+
+        </View>
+
+    )
+}
---
+++ app/Tela Inicial IFome/index.jsx
@@ -0,0 +1,34 @@
+import { Text, View, StyleSheet, FlatList, Pressable, Image } from "react-native";
+import { AppContext } from "../../scripts/AppContext";
+import { useContext, useEffect } from "react";
+import ItemIfome from "./ItemIfome";
+
+const styles = StyleSheet.create({
+    lista: {
+        gap: 10,
+    }
+})
+const PRODUTOS = [
+    {id: 1, nome: "Coxinha", local: "Kobrasol", preco: 3.80, imagem: "https://www.imagensempng.com.br/wp-content/uploads/2023/04/474.png", data: "02/08/2024"},
+    {id: 2, nome: "BigMac", local: "Shopping", preco: 32.80, imagem: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kzXCTbnv/200/200/original?country=br", data: "02/08/2024"},
+]
+export default TelaInicialIfome = ({navigation}) => {
+    const { carrinho, setCarrinho } = useContext(AppContext)
+    return (
+            <View style={styles.container}>
+                <Pressable onPress={() => navigation.navigate('Carrinho')} style={{display: "flex", flexDirection: 'row', alignItems: 'center', alignSelf: 'flex-end', marginTop: 10, marginRight: 10}}>
+                    <Image source={{uri: "https://cdn-icons-png.flaticon.com/512/6493/6493694.png"}} style={{width: 30, height: 30}}/>
+                    <Text style={{color: "#ff0000"}}>{carrinho.length} itens</Text>
+                </Pressable>
+                <FlatList
+                    data={PRODUTOS}
+                    renderItem={({ item }) => <ItemIfome imagem={item.imagem} local={item.local} nome={item.nome} preco={item.preco} />}
+                    keyExtractor={item => item.id}
+                    style={styles.lista}
+                />
+
+            </View>
+
+
+    )
+}
